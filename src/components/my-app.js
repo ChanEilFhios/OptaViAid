e250430a756267eefbe2b781476e9966d770280f
Brendan O'Brien <bobrien@gmail.com>
2018-07-05 20:41:03 -0400

    cleaning up unneeded code features

--- src/components/my-app.js
+++ src/components/my-app.js
@@ -11,7 +11,6 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
 import { LitElement, html } from '@polymer/lit-element';
 import { setPassiveTouchGestures } from '@polymer/polymer/lib/utils/settings.js';
 import { connect } from 'pwa-helpers/connect-mixin.js';
-import { installMediaQueryWatcher } from 'pwa-helpers/media-query.js';
 import { installOfflineWatcher } from 'pwa-helpers/network.js';
 import { installRouter } from 'pwa-helpers/router.js';
 import { updateMetadata } from 'pwa-helpers/metadata.js';
@@ -22,12 +21,8 @@ import { store } from '../store.js';
 // These are the actions needed by this element.
 import {
   navigate,
-  updateOffline,
-  updateLayout
+  updateOffline
 } from '../actions/app.js';
-
-// These are the elements needed by this element.
-import './snack-bar.js';
 
 class MyApp extends connect(store)(LitElement) {
   _render({appTitle, _page, _snackbarOpened, _offline}) {
@@ -73,6 +68,7 @@ class MyApp extends connect(store)(LitElement) {
         overflow: scroll;
         display: block; /* Workaround for IE11 displaying <main> as inline */
         grid-area: main;
+        padding-right: 0.25em;
       }
 
       .page {
@@ -111,33 +107,24 @@ class MyApp extends connect(store)(LitElement) {
     </header>
 
     <nav class="toolbar-list">
-      <a selected?="${_page === 'view1'}" href="/view1">View One</a>|
-      <a selected?="${_page === 'view2'}" href="/view2">View Two</a>|
-      <a selected?="${_page === 'view3'}" href="/view3">View Three</a>
+      <a selected?="${_page === 'view1'}" href="/view1">View One</a>
     </nav>
 
     <!-- Main content -->
     <main role="main" class="main-content">
       <my-view1 class="page" active?="${_page === 'view1'}"></my-view1>
-      <my-view2 class="page" active?="${_page === 'view2'}"></my-view2>
-      <my-view3 class="page" active?="${_page === 'view3'}"></my-view3>
       <my-view404 class="page" active?="${_page === 'view404'}"></my-view404>
     </main>
 
     <footer>
-      <p>Made with &hearts; by the Polymer team.</p>
-    </footer>
-
-    <snack-bar active?="${_snackbarOpened}">
-        You are now ${_offline ? 'offline' : 'online'}.</snack-bar>
-    `;
+      You are now ${_offline ? 'offline' : 'online'}.</snack-bar>
+    </footer>`
   }
 
   static get properties() {
     return {
       appTitle: String,
       _page: String,
-      _snackbarOpened: Boolean,
       _offline: Boolean
     }
   }
@@ -145,8 +132,6 @@ class MyApp extends connect(store)(LitElement) {
   _firstRendered() {
     installRouter((location) => store.dispatch(navigate(window.decodeURIComponent(location.pathname))));
     installOfflineWatcher((offline) => store.dispatch(updateOffline(offline)));
-    installMediaQueryWatcher(`(min-width: 460px)`,
-        (matches) => store.dispatch(updateLayout(matches)));
   }
 
   _didRender(properties, changeList) {
@@ -163,7 +148,6 @@ class MyApp extends connect(store)(LitElement) {
   _stateChanged(state) {
     this._page = state.app.page;
     this._offline = state.app.offline;
-    this._snackbarOpened = state.app.snackbarOpened;
   }
 }
 
